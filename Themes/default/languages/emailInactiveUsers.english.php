dcc4ef21e379b97399854e8f49c4ce89139ec622
Suki <suki@missallsunday.com>
2014-03-07 20:08:37 -0600

    language file
    Signed-off-by: Suki <suki@missallsunday.com>

---
+++ Themes/default/languages/emailInactiveUsers.english.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * email Inactive Users
+ *
+ * @package eIU mod
+ * @version 1.0
+ * @author Jessica González <suki@missallsunday.com>
+ * @copyright Copyright (c) 2014 Jessica González
+ * @license http://www.mozilla.org/MPL/2.0/
+ */
+
+global $txt;
+
+$txt['emailInactiveUsers_'] = '';
