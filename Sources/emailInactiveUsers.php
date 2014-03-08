b8795eb354cecabc433b0e09702712233a6bd3f2
Suki <suki@missallsunday.com>
2014-03-07 19:44:37 -0600

    First files
    Signed-off-by: Suki <suki@missallsunday.com>

---
+++ Sources/emailInactiveUsers.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * email Inactive Users
+ *
+ * @package eIU mod
+ * @version 1.0
+ * @author Jessica Gonzalez <suki@missallsunday.com>
+ * @copyright Copyright (c) 2014 Jessica González
+ * @license http://www.mozilla.org/MPL/2.0/
+ */
+
+if (!defined('SMF'))
+	die('No direct access...');
