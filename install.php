5dead9d009ea18fe4385a55f72643f1ad9dd5c41
Suki <suki@missallsunday.com>
2014-03-09 12:42:33 -0600

    Add a cehck for php 5.3
    Signed-off-by: Suki <suki@missallsunday.com>

--- install.php
+++ install.php
@@ -19,6 +19,8 @@ elseif (!defined('SMF'))
 
 global $smcFunc, $context;
 
+eiu_check();
+
 db_extend('packages');
 
 if (empty($context['uninstalling']))
@@ -56,4 +58,10 @@ if (empty($context['uninstalling']))
 			'id_task',
 		)
 	);
+}
+
+function eiu_check();
+{
+	if (version_compare(PHP_VERSION, '5.3.0', '<'))
+		fatal_error('This mod needs PHP 5.3 or greater. You will not be able to install/use this mod, contact your host and ask for a php upgrade.');
 }
