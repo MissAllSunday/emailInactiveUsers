6abe705d4bb374088d7761fec3fa4bf9a2f11d73
Suki <suki@missallsunday.com>
2014-03-09 12:49:16 -0600

    LOL
    Signed-off-by: Suki <suki@missallsunday.com>

--- install.php
+++ install.php
@@ -60,7 +60,7 @@ if (empty($context['uninstalling']))
 	);
 }
 
-function eiu_check();
+function eiu_check()
 {
 	if (version_compare(PHP_VERSION, '5.3.0', '<'))
 		fatal_error('This mod needs PHP 5.3 or greater. You will not be able to install/use this mod, contact your host and ask for a php upgrade.');
